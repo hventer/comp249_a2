e2db4506a3a3c196e310cdd7fdd8b43d0d0c7dc5
hannesventer <hannes.venter@students.mq.edu.au>
2019-04-28 10:31:18 +1000

    Added comments

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ app = Bottle()
 
 @app.route('/')
 def index(db):
+    """The home page. Will create/get cookie.
+    Returns the index file with the title and
+    the list of all products in the store.
+    Index will iterate over the list and display
+    each product.
+    """
     session.get_or_create_session(db)
 
     info = {
@@ -21,29 +27,46 @@ def index(db):
 
 @app.route('/product/<id>')
 def product(db, id):
+    """The product page. Will create/get cookie
+    When a product is clicked on, this page
+    will diplay custom info for that product.
+    If the id does not match an id in the db,
+    return 404 not found
+    """
     session.get_or_create_session(db)
 
     product = model.product_get(db, id)
 
     if product:
+        #this product exists, to get its info.
         info = {
             'product': model.product_get(db, id)
         }
         return template('product', info)
     else:
+        #no product exists, return 404
         return HTTPError(404, "No such product.")
 
 
 @app.route('/cart')
 def cart(db):
+    """The GET cart page. Will create/get cookie
+    When '/cart' is navigated to, this page will
+    be displayed. It retrieves all the contents
+    of the cart. The cart.html file will iterate
+    over the items and display them.
+    Also calculates the total amount
+    """
     session.get_or_create_session(db)
 
     cart = session.get_cart_contents(db)
 
+    #iterate over cart and add up the costs
     total = 0
     for item in cart:
         total=total+item['cost']
 
+    #pass the cart and total to the cart.html
     info = {
         'title': "Current Shopping Cart contents",
         'cart': cart,
@@ -53,6 +76,13 @@ def cart(db):
 
 @app.post('/cart')
 def postcart(db):
+    """The POST cart page. Will create/get cookie
+    When submit is pressed on a product page, this
+    will run. It will get the product id and quantity
+    selected from the form. It will then add the id
+    and quantity to the cart.
+    Finishes by redirecting to the GET cart page
+    """
     session.get_or_create_session(db)
 
     product = request.forms.get('product')
--- session.py
+++ session.py
@@ -21,22 +21,26 @@ def get_or_create_session(db):
 
     Returns the session key (string)
     """
-
+    #retrieve the current cookie (none is there isnt one)
     sessionkey = request.get_cookie(COOKIE_NAME)
 
+    #find the corresponding sessionid in the sessions database
     cursor = db.cursor()
     sql = "SELECT sessionid FROM sessions WHERE sessionid=?"
     cursor.execute(sql, (sessionkey,))
 
     row = cursor.fetchone()
     if not row:
-        # no existing session so we create a new one
+        # no existing session so create a new one
         sessionkey = uuid.uuid4().hex
         cursor = db.cursor()
+
+        #place the new session key into sessions db
         sql = "INSERT INTO sessions (sessionid) VALUES (?)"
         cursor.execute(sql, (sessionkey,))
         db.commit()
 
+        #set the cookie with new sessionkey and path '/'
         response.set_cookie(COOKIE_NAME, sessionkey, path='/')
     return sessionkey
 
@@ -44,25 +48,38 @@ def get_or_create_session(db):
 def add_to_cart(db, itemid, quantity):
     """Add an item to the shopping cart"""
 
+    #retrieve a session key
     sessionkey = get_or_create_session(db)
 
+    #find any existing data in sessions db
     sql = "SELECT data FROM sessions WHERE sessionid = ?"
     cursor = db.cursor()
     cursor.execute(sql, (sessionkey,))
     row = cursor.fetchone()
 
     if row['data']:
+        #there is already data in sessions, so append new data to sessions db
         data = [itemid, quantity]
+
+        #retrieve old data (ie. [[itemid, quantity]])
         olddata = json.loads(row['data'])
+
+        #append new data
         olddata.append(data)
+
+        # create data text (ie. '[[itemid, quantity], [itemid, quantity]]')
         data_j = json.dumps(olddata)
 
+        #update the sessions db
         sql = "UPDATE sessions SET data = ? WHERE sessionid = ?"
         cursor.execute(sql, [data_j, sessionkey])
     else:
+        #this is the first item in the cart
+        #create a list within a list
         data = [[itemid, quantity]]
         data_j = json.dumps(data)
 
+        # update the sessions db
         sql = "UPDATE sessions SET data = ? WHERE sessionid = ?"
         cursor.execute(sql, [data_j, sessionkey])
 
@@ -74,26 +91,31 @@ def get_cart_contents(db):
     a list of dictionaries:
     [{'id': <id>, 'quantity': <qty>, 'name': <name>, 'cost': <cost>}, ...]
     """
+    #retrieve a session key
     sessionkey = get_or_create_session(db)
 
+    #find existing data
     sql = "SELECT data FROM sessions WHERE sessionid = ?"
     cursor = db.cursor()
     cursor.execute(sql, (sessionkey,))
 
     row = cursor.fetchone()
     if row['data']:
+        #there is something in the cart
         list = json.loads(row['data'])
         cart = []
 
         for item in list:
+            #each 'item' is an id and qty. Find each product using id
             product = model.product_get(db, item[0])
             name = product['name']
             cost = product['unit_cost']
 
             id = item[0]
-            quantity = int(item[1])
-            cost = cost * float(quantity)
+            quantity = int(item[1]) #must be int to pass test in views
+            cost = cost * float(quantity) #must be float to be multiplied by float
 
+            #add all data to dictionary
             item = {
                 'id': id,
                 'quantity': quantity,
@@ -101,9 +123,11 @@ def get_cart_contents(db):
                 'cost': cost
             }
 
+            #add dictionary to cart
             cart.append(item)
 
         return cart
 
     else:
+        #cart is empty
         return []
